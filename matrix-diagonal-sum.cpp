81b007bea70bb04ed62486d1dad47c2315c94815
touro3 <tourinholucas123@gmail.com>
2024-09-16 08:43:18 -0300

    add matrix-diagonal-sum.cpp

---
+++ matrix-diagonal-sum.cpp
@@ -0,0 +1,22 @@
+//resolved by Lucas Tourinho Mamede
+//time complexity O(n)
+//space complexity O(1)
+
+#include <bits/stdc++>
+
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+
+        for (int i = 0; i < n; ++i) {
+            sum += mat[i][i]; 
+            if (i != n - i - 1) { 
+                sum += mat[i][n - i - 1];
+            }
+        }
+        return sum;
+    }
+};
+
