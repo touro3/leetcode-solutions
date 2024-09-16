a94627be665a3b99c5cda222cd73b8501e68e4de
touro3 <tourinholucas123@gmail.com>
2024-09-16 08:52:59 -0300

    add count-servers-that-communicate.cpp

---
+++ count-servers-that-communicate.cpp
@@ -0,0 +1,26 @@
+//resolved by Lucas Tourinho Mamede
+//time complexity O(n*m)
+//space complexity O(n+m)
+
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        vector<int> rows(grid.size()), columns(grid[0].size());
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                if (grid[i][j]) {
+                    ++rows[i], ++columns[j];
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                if (grid[i][j] && (rows[i] > 1 || columns[j] > 1)) {
+                    ++result;
+                }
+            }
+        }
+        return result;
+    }
+};
